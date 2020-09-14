f47c231be7b811808e509381d86cdce812803730
Devadutta <devadutta.dash.cd.cse19@itbhu.ac.in>
2020-09-14 10:59:17 +0530

    Finished my blog functionality

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
 	title = models.CharField(max_length=500)
 	body = models.TextField()
 	published_date = models.DateTimeField()
+
+	def  __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:90]
+
+	def pub_date_pretty(self):
+		return self.published_date.strftime('%b %e %Y')	
